2da2cf0fc864400f6bb53157644f8758fdcff6e6
StevenSerrien <steven.serrien@student.kdg.be>
2016-12-21 20:49:20 +0100

    styling sidemenu

--- resources/views/includes/sidemenu.blade.php
+++ resources/views/includes/sidemenu.blade.php
@@ -1,13 +1,28 @@
 <nav id="sidemenu" class="sidemenu-collapsed">
   <ul>
-    <li class='hamburger-icon'></li>
+    <a href="#" class='sidemenu-item'>
+      <li class='hamburger-icon'></li>
+    <a href="#" class='sidemenu-item light'>
     <li class='search-icon'></li>
+    </a>
+    <a href="#" class='sidemenu-item light'>
     <li class='faq-icon'></li>
+    </a>
+    <a href="#" class='sidemenu-item'>
     <li class='dog-icon'></li>
+    </a>
+    <a href="#" class='sidemenu-item'>
     <li class='cat-icon'></li>
+    </a>
+    <a href="#" class='sidemenu-item'>
     <li class='fish-icon'></li>
+    </a>
+    <a href="#" class='sidemenu-item'>
     <li class='owl-icon'></li>
+    </a>
+    <a href="#" class='sidemenu-item'>
     <li class='hamster-icon'></li>
+    </a>
   </ul>
   <span class='brand-name collapsed-element'>
     K
