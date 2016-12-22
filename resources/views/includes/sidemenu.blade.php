71b14fbb16973035aeecb99c837f2b6916b98449
StevenSerrien <steven.serrien@student.kdg.be>
2016-12-22 12:06:38 +0100

    Added some text if menu is expanded

--- resources/views/includes/sidemenu.blade.php
+++ resources/views/includes/sidemenu.blade.php
@@ -3,25 +3,25 @@
     <a href="#" class='sidemenu-item m-expand'>
       <li class='hamburger-icon'></li>
     <a href="#" class='sidemenu-item light'>
-    <li class='search-icon'></li>
+    <li class='search-icon'></li><span class="expanded-element alternative">Search</span>
     </a>
     <a href="#" class='sidemenu-item light'>
-    <li class='faq-icon'></li>
+    <li class='faq-icon'></li><span class="expanded-element alternative">FAQ</span>
     </a>
     <a href="#" class='sidemenu-item'>
-    <li class='dog-icon'></li>
+    <li class='dog-icon'></li><span class="expanded-element">Dogs</span>
     </a>
     <a href="#" class='sidemenu-item'>
-    <li class='cat-icon'></li>
+    <li class='cat-icon'></li><span class="expanded-element">Cats</span>
     </a>
     <a href="#" class='sidemenu-item'>
-    <li class='fish-icon'></li>
+    <li class='fish-icon'></li><span class="expanded-element">Fish</span>
     </a>
     <a href="#" class='sidemenu-item'>
-    <li class='owl-icon'></li>
+    <li class='owl-icon'></li><span class="expanded-element">Birds</span>
     </a>
     <a href="#" class='sidemenu-item'>
-    <li class='hamster-icon'></li>
+    <li class='hamster-icon'></li><span class="expanded-element">Small animals</span>
     </a>
   </ul>
   <span class='brand-name collapsed-element'>
