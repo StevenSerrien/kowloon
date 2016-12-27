6f3195c1abcc214bfb8344dcd5d8ce8f4bbb5c3b
StevenSerrien <steven.serrien@student.kdg.be>
2016-12-27 18:20:14 +0100

    category + sprites + borders

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -15,5 +15,42 @@
 @endsection
 
 @section('content')
-    <p>This is my body content.</p>
+  <div class="row">
+    <p class="intro-text">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    <div class="home-categories clearfix">
+    <div class="col-xs-6 col-md-2">
+      <div class="kowloon-big-dogs">
+      </div>
+      <h2>DOGS</h2>
+    </div>
+    <div class="col-xs-6 col-md-2">
+      <div class="kowloon-big-cats">
+      </div>
+      <h2>CATS</h2>
+    </div>
+    <div class="col-xs-6 col-md-2">
+      <div class="kowloon-big-fish">
+      </div>
+      <h2>FISH</h2>
+    </div>
+    <div class="col-xs-6 col-md-2">
+      <div class="kowloon-big-birds">
+      </div>
+      <h2>BIRDS</h2>
+    </div>
+    <div class="col-xs-6 col-md-2">
+      <div class="kowloon-big-hamster">
+      </div>
+      <h2>SMALL ANIMALS</h2>
+    </div>
+    <div class="col-xs-6 col-md-2">
+      <div class="kowloon-big-plus">
+      </div>
+      <h2>OTHER</h2>
+    </div>
+    </div>
+  </div>
 @endsection
