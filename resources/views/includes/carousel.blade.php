8c44c5735c923859e5dcc765c6db0c17795c1151
StevenSerrien <steven.serrien@student.kdg.be>
2016-12-27 13:24:48 +0100

    static logo in carousel + black indicator

--- resources/views/includes/carousel.blade.php
+++ resources/views/includes/carousel.blade.php
@@ -1,19 +1,24 @@
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Static Logo -->
+  <div class="carousel-logo">
+    <img src="{{ asset('images/kowloon-logo.png') }}" alt="kowloon logo">
+  </div>
+
   <!-- Indicators -->
   <ol class="carousel-indicators">
     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
     <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li class='carousel-indicator-disabled' data-target="#myCarousel" data-slide-to="2"></li>
   </ol>
 
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
     <div class="item active">
-      <img src="{{ asset('images/carousel/kowloon-hero-1.png') }}" alt="Chania">
+      <img src="{{ asset('images/carousel/kowloon-hero-1.png') }}" alt="Big promotional hero image - first">
     </div>
 
     <div class="item">
-      <img src="{{ asset('images/carousel/kowloon-hero-2.png') }}" alt="Chania">
+      <img src="{{ asset('images/carousel/kowloon-hero-2.png') }}" alt="Big promotional hero image - second">
     </div>
   </div>
 
