fb94de0d82a602f9464d873c3200287defd29f3f
StevenSerrien <steven.serrien@student.kdg.be>
2017-01-10 16:36:36 +0100

    started content on search page

--- resources/views/includes/search.blade.php
+++ resources/views/includes/search.blade.php
@@ -1,22 +1,30 @@
-<div id="morph-search-faq" class="morph-search-faq">
+<div id="morph-search-faq" class="morph-search-faq open">
   <span id="m-close-button" class='m-close'></span>
-  <!-- Search section -->
-  <div id="m-search" class="">
-    <form class="morphsearch-form">
+  <div class="container">
+    <div id="m-search" class="m-active">
+      <a class='advanced-filter pointer no-decoration' data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+        Advanced filter
+      </a>
+      <form class="morphsearch-form">
 
-    </form>
+      </form>
+    </div>
+    <!-- end of Search section -->
+    <!-- FAQ section -->
+    <div id="m-faq" class="">
+    </div>
+    <!-- end of FAQ section -->
   </div>
-  <!-- end of Search section -->
-  <!-- FAQ section -->
-  <div id="m-faq" class="">
+  <!-- Search section -->
+
     {{-- <a class="btn btn-primary" role="button" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
   Link with href
-</a>
+
 <div class="collapse" id="collapseExample">
   <div class="well">
     ...
   </div>
 </div> --}}
-  </div>
-  <!-- end of FAQ section -->
+
+
 </div>
