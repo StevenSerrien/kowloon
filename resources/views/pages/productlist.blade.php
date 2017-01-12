85e069a6eef0b9db3ef7f8985d3d1a14b6415df2
StevenSerrien <steven.serrien@student.kdg.be>
2017-01-12 16:23:42 +0100

    added colors to each tag when active

--- resources/views/pages/productlist.blade.php
+++ resources/views/pages/productlist.blade.php
@@ -6,7 +6,10 @@
 @section('content')
   <div class='productlist-section'>
   <div class="row">
-    <span class='branding-letter'>K</span><tag><span class='circle'></span>about us</tag><tag>Splash 'n Fun</tag>
+    <span class='branding-letter'>K</span><tag><span class='circle {{Request::segment(2)}}'></span>{{Request::segment(2)}}</tag><tag>Splash 'n Fun</tag>
+  </div>
+  <div class="row">
+  <h1 class='headtitle'>{{Request::segment(2)}} ARTICLES.</h1>
   </div>
   </div>
 @endsection
