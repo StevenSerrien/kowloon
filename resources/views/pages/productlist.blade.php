3cea6a63b1dc679e3561bcf302fcc5938178319c
StevenSerrien <steven.serrien@student.kdg.be>
2017-01-12 17:47:00 +0100

    productlist left products ready

--- resources/views/pages/productlist.blade.php
+++ resources/views/pages/productlist.blade.php
@@ -61,6 +61,109 @@
         </div>
       </form>
     </div>
+
+
+  </div>
+  <!-- END OF FILTER -->
+  <hr class='colored'>
+  <div class="row">
+  <div class="productlist-grid hot-items clearfix">
+    <div class="col-md-6 col-sm-12 outer-productlist">
+      <div class="col-xs-6">
+        <div class="h-item clearfix">
+          <div class="img" style="background-image:url({{ URL::asset('images/hot-item-dog.png') }})">
+            <ol class="hot-item-indicators">
+              <li class="h-item-indicator-1"></li>
+              <li class="h-item-indicator-2"></li>
+              <li class="h-item-indicator-3"></li>
+            </ol>
+            <div class="img-overlay">
+              <div class="icon-details">
+                <span>view details</span>
+              </div>
+
+              </div>
+          </div>
+
+          <div class='h-item-textbox'>
+            <span class="h-item-name bold">Cooling mat</span>
+            <span class="h-item-price">€ 15,49</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-6">
+        <div class="h-item clearfix">
+          <div class="img" style="background-image:url({{ URL::asset('images/hot-item-dog.png') }})">
+            <ol class="hot-item-indicators">
+              <li class="h-item-indicator-1"></li>
+              <li class="h-item-indicator-2"></li>
+              <li class="h-item-indicator-3"></li>
+            </ol>
+            <div class="img-overlay">
+              <div class="icon-details">
+                <span>view details</span>
+              </div>
+
+              </div>
+          </div>
+
+          <div class='h-item-textbox'>
+            <span class="h-item-name bold">Cooling mat</span>
+            <span class="h-item-price">€ 15,49</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-6">
+        <div class="h-item clearfix">
+          <div class="img" style="background-image:url({{ URL::asset('images/hot-item-dog.png') }})">
+            <ol class="hot-item-indicators">
+              <li class="h-item-indicator-1"></li>
+              <li class="h-item-indicator-2"></li>
+              <li class="h-item-indicator-3"></li>
+            </ol>
+            <div class="img-overlay">
+              <div class="icon-details">
+                <span>view details</span>
+              </div>
+
+              </div>
+          </div>
+
+          <div class='h-item-textbox'>
+            <span class="h-item-name bold">Cooling mat</span>
+            <span class="h-item-price">€ 15,49</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-6">
+        <div class="item-stack">
+        <div class="h-item stacked stack-1"></div>
+        <div class="h-item stacked stack-2"></div>
+        <div class="h-item stacked clearfix">
+          <div class="img" style="background-image:url({{ URL::asset('images/hot-item-dog.png') }})">
+            <div class="number-count">
+              <span class='bold'>4</span>
+            </div>
+            <div class="img-overlay">
+              <div class="icon-details">
+                <span>view details</span>
+              </div>
+
+              </div>
+          </div>
+
+          <div class='h-item-textbox'>
+            <span class="h-item-name bold">Cooling mat</span>
+            <span class="h-item-price">€ 15,49</span>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+    <div class="col-md-6 col-sm-12 outer-productlist">
+
+    </div>
+  </div>
   </div>
   </div>
   </div>
