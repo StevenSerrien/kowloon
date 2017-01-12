fe9b50ab12b12e90f37c04462a632745e2c4bfcf
StevenSerrien <steven.serrien@student.kdg.be>
2017-01-12 15:53:21 +0100

    added circle to tags

--- resources/views/pages/productlist.blade.php
+++ resources/views/pages/productlist.blade.php
@@ -4,5 +4,9 @@
   @include('includes.carousel')
 @endsection
 @section('content')
-
+  <div class='productlist-section'>
+  <div class="row">
+    <span class='branding-letter'>K</span><tag><span class='circle'></span>about us</tag><tag>Splash 'n Fun</tag>
+  </div>
+  </div>
 @endsection
