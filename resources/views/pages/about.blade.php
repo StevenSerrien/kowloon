159c2fd8457208736e368d464538de584c892641
StevenSerrien <steven.serrien@student.kdg.be>
2017-01-13 20:59:27 +0100

    secure added - finale versie

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -7,11 +7,11 @@
   <div id="big-hero-aboutus" class="carousel slide">
     <!-- Static Logo -->
     <div class="carousel-logo">
-      <img src="{{ asset('images/kowloon-logo.png') }}" alt="kowloon logo">
+      <img src="{{ secure_asset('images/kowloon-logo.png') }}" alt="kowloon logo">
     </div>
     <div class="carousel-inner" role="listbox">
       <div class="item active">
-        <img src="{{ asset('images/aboutus-hero.png') }}" alt="Big promotional hero image - first">
+        <img src="{{ secure_asset('images/aboutus-hero.png') }}" alt="Big promotional hero image - first">
       </div>
     </div>
   </div>
